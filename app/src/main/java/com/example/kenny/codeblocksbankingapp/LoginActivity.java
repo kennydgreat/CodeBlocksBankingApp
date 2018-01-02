9b75e161f3a6d08572b0915a3ef65bfc40ef52c6
Kenneth Uyabeme <kuyabeme@gmail.com>
2018-01-02 16:54:46 -0500

    -Main user page UI created along with nagivation drawer -The activity for the main user page was created -All functionality for opening the nagivation drawer has been implemented -Implementung the Navigation drawer required changes to the gradle file

--- app/src/main/java/com/example/kenny/codeblocksbankingapp/LoginActivity.java
+++ app/src/main/java/com/example/kenny/codeblocksbankingapp/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.kenny.codeblocksbankingapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
@@ -17,6 +18,10 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //This to test MainUserPageActivity remove once we get this activity working
+        //and properly launching MainUserPageActivity after user authentication
+        Intent intent = new Intent(getApplicationContext(), MainUserPageActivity.class);
+        startActivity(intent);
 
     }
 }
