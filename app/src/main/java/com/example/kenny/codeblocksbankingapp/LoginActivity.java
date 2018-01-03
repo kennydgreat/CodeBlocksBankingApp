7ee20e1d6fd52bce5b9ff997a078716e41486382
Kenneth Uyabeme <kuyabeme@gmail.com>
2018-01-02 19:54:16 -0500

    -"TaskDescription's primary color should be opaque" UI bug fixed -UI account display on main user page adjusted for more spacing

--- app/src/main/java/com/example/kenny/codeblocksbankingapp/LoginActivity.java
+++ app/src/main/java/com/example/kenny/codeblocksbankingapp/LoginActivity.java
@@ -2,12 +2,9 @@ package com.example.kenny.codeblocksbankingapp;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class LoginActivity extends Activity {
 
@@ -20,9 +17,9 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //This to test MainUserPageActivity remove once we get this activity working
-        //and properly launching MainUserPageActivity after user authentication
-        Intent intent = new Intent(getApplicationContext(), MainUserPageActivity.class);
+        //This to test BankActivity remove once we get this activity working
+        //and properly launching BankActivity after user authentication
+        Intent intent = new Intent(getApplicationContext(), BankActivity.class);
         startActivity(intent);
 
         edtAccessCardNo = findViewById(R.id.edt_accessCard);
